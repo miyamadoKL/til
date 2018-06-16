e2a808ca5648c9ec2b73054c0ac444648e40ed6d
miyamadoKL <105kansai500@gmail.com>
2018-06-16 23:48:03 +0900

    commited on June 16

--- til_20180615/symbol.rb
+++ til_20180615/symbol.rb
@@ -1,1 +1,24 @@
-p :apple.class
+def buy_burger(menu, drink: true, potato: true)
+  puts menu
+
+  if drink
+    puts 'Drink'
+  end
+
+  if potato
+    puts 'Potato'
+  end
+end
+
+buy_burger('fish')
+
+person = {
+  name: 'Alice',
+  age: 20,
+  friends: ['Bob', 'Carol'],
+  phones: { home: '1234-0000', mobile: '5678-0000' }
+}
+
+puts person[:age]
+puts person[:friends]
+puts person[:phones][:mobile]
