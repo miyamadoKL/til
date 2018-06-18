5b9ea697288466c4a28fc47ef0a30f9e195900a8
miyamadoKL <105kansai500@gmail.com>
2018-06-18 23:00:45 +0900

    Added contents from chapter 6.3

--- til_20180618/regexp1.rb
+++ til_20180618/regexp1.rb
@@ -4,14 +4,14 @@ Python is a great language.
 Java and Javascript are different.
 TEXT
 
-p text.scan(/[A-Z][A-Za-z]+/)
+p text.scan(/[A-Z][A-Za-z]+/) #=> ["Ruby", "Python", "Java", "Javascript"]
 
 text = <<TEXT
 私の郵便番号は1234567です。
 僕所住所は6777056 兵庫県西脇市板波町1234だよ。
 TEXT
 
-puts text.gsub(/(\d{3})(\d{4})/, '\1-\2')
+puts text.gsub(/(\d{3})(\d{4})/, '\1-\2') #=> 123-4567, 677-7056...
 
 p '0123-4567' =~ /\d{3}-\d{4}/ #=> 1
 p 'Hello' =~ /\d{3}-\d{4}/ #=> nil
@@ -21,3 +21,25 @@ if 'Scala' =~ /[A-Z][A-Za-z]+/
 else
   puts 'Not Matched'
 end
+
+text = '私の誕生日は1977年7月17日です。'
+m = /(\d+)年(\d+)月(\d+)日/.match(text)
+p m[0] #=> "1977年7月17日"
+p m[1] #=> "1977"
+p m[2] #=> "7"
+p m[3] #=> "17"
+p m[2, 2] #=> ["7", "17"]
+
+m2 = text.match(/(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/)
+p m2[:year] #=> "1977"
+
+if /(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/ =~ text
+  puts "#{year}/#{month}/#{day}" #=> 1977/7/17
+end
+
+p '12345 678 9'.scan(/\d+/) #=> ["12345, "678", "9"]
+
+p '1977年7月17日 2016年12月31日'.scan(/(?:\d+)年(?:\d+)月(?:\d+)日/) #=> ["1977年7月17日", "2016年12月31日"]
+
+text = '郵便番号は123-4567です'
+puts text[/\d{3}-\d{4}/] #=> 123-4567
